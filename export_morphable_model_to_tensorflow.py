7a64b74cc0efb24bd9e72657f4d98f8c57f51131
rickshilling <rick.shilling@gmail.com>
2017-09-11 17:10:47 -0400

    Add files via upload

--- export_morphable_model_to_tensorflow.py
+++ export_morphable_model_to_tensorflow.py
@@ -1,6 +1,11 @@
 import eos
+import tensorflow as tf
 
 model_file_name = "../../share/sfm_shape_3448.bin"
 
 model = eos.morphablemodel.load_model(model_file_name)
 shape_model = model.get_shape_model()
+shape_model_pca_basis = tf.Variable(shape_model.get_orthonormal_pca_basis(), name='PCA_basis')
+
+pca_matrix_size = shape_model_pca_basis.get_shape().as_list()
+x = tf.Variable(tf.random_normal([pca_matrix_size[1], 1], 0.01), name='coefficients')
