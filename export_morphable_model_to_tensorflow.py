63048098622ab887c73a359e599b5b5d8dfd5841
rickshilling <rick.shilling@gmail.com>
2017-09-11 22:46:47 -0400

    Add files via upload

--- export_morphable_model_to_tensorflow.py
+++ export_morphable_model_to_tensorflow.py
@@ -8,4 +8,14 @@ shape_model = model.get_shape_model()
 shape_model_pca_basis = tf.Variable(shape_model.get_orthonormal_pca_basis(), name='PCA_basis')
 
 pca_matrix_size = shape_model_pca_basis.get_shape().as_list()
-x = tf.Variable(tf.random_normal([pca_matrix_size[1], 1], 0.01), name='coefficients')
+
+# Model & estimate
+x = tf.Variable(tf.random_normal([pca_matrix_size[1], 1], stddev=1e-2), name='coefficients')
+b = tf.matmul(shape_model_pca_basis,x)
+x_hat = tf.matrix_solve_ls(shape_model_pca_basis,b)
+loss = tf.norm(x-x_hat)
+model = tf.global_variables_initializer()
+with tf.Session() as session:
+    session.run(model)
+    print(session.run(loss))
+
