81fdc0efb3d95a8660d4114545aff2121a96a7d9
rickshilling <rick.shilling@gmail.com>
2017-09-27 20:48:50 -0400

    Add files via upload

--- export_morphable_model_to_tensorflow.py
+++ export_morphable_model_to_tensorflow.py
@@ -4,11 +4,24 @@ import tensorflow as tf
 
 model_file_name = "../../share/sfm_shape_3448.bin"
 
-model = eos.morphablemodel.load_model(model_file_name)
-shape_model = model.get_shape_model()
-shape_model_pca_basis = tf.Variable(shape_model.get_rescaled_pca_basis(), name='PCA_shape')
+def read_pts(filename):
+    """A helper function to read ibug .pts landmarks from a file."""
+    lines = open(filename).read().splitlines()
+    lines = lines[3:71]
+
+    landmarks = []
+    for l in lines:
+        coords = l.split()
+        landmarks.append([float(coords[0]), float(coords[1])])
+
+    return landmarks
+
+morphable_model = eos.morphablemodel.load_model(model_file_name)
+shape_model = morphable_model.get_shape_model()
+shape_model_pca_basis = tf.Variable(shape_model.get_rescaled_pca_basis(), name='PCA_shape1')
 
 pca_shape_matrix_size = shape_model_pca_basis.get_shape().as_list()
+num_points = pca_shape_matrix_size[0]/3
     
 mean = shape_model.get_mean()
 v_bar = tf.Variable(mean.reshape([mean.shape[0], 1]),name='shape_mean')
@@ -16,7 +29,23 @@ alpha = tf.Variable(tf.random_normal([pca_shape_matrix_size[1], 1], stddev=1e-2)
 v = tf.matmul(shape_model_pca_basis,alpha) + v_bar
 alpha_hat = tf.matrix_solve_ls(shape_model_pca_basis,v-v_bar)
 loss = tf.norm(alpha-alpha_hat)
+
+ds = tf.reshape(v,[3,num_points])
+ds_1 = tf.concat([ds, tf.ones([1,num_points])], 0)
+focal_length = 2 # TODO: Make tf.Variable
+projection_matrix = tf.Variable([[1.,0.,0.,0.],[0.,1.,0.,0.],[0.,0.,1.,0.],[0.,0.,1./focal_length,0.]])
+f = tf.matmul(projection_matrix,ds_1)
+b_0 = tf.divide(f[0],f[3])
+b_1 = tf.divide(f[1],f[3])
+b = tf.concat([b_0, b_1], 0)
+
 global_model = tf.global_variables_initializer()
 with tf.Session() as session:
     session.run(global_model)
     print(session.run(loss))
+    print(session.run(f))
+    print(session.run(b))
+
+tf.reset_default_graph()
+session.close()
+             
